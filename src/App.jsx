bc3c9a8e653002fe9e25e24022d42af32337aab6
Ayush Porwal <ping.ayushporwal@gmail.com>
2023-10-19 23:44:13 +0530

    reactv18 webpackv5 template commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+function App() {
+    return (
+        <div>
+            <h1 className="webpage_title">
+                Welcome to my React's webpack template
+            </h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    devServer: {
+        port: 3000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            [
+                                '@babel/preset-react',
+                                {
+                                    runtime: 'automatic',
+                                },
+                            ],
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
+};
